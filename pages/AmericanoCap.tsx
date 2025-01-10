aa2d4784898d9b0eda686ab00c41aec8c77f0914
JacoT1989-dotcom <jacothiart52@gmail.com>
2025-01-10 17:50:07 +0200

    update password reset env

--- pages/AmericanoCap.tsx
+++ pages/AmericanoCap.tsx
@@ -1,5 +1,5 @@
-"use client"
-
+"use client";
+//
 import { RiStarSFill } from "react-icons/ri";
 import Image from "next/image";
 import Link from "next/link";
@@ -20,30 +20,32 @@ const AmericanoCap = () => {
     <main className="py-3">
       <div className="card card-compact rounded-md object-cover w-90">
         <figure>
-        <Link href="/products/americano-cap">
-          <Image
-            src={
-              isHovered
-                ? "/Americano-Royal-Foc1-scaled.jpg"
-                : "/Americano-model-scaled.jpg"
-            }
-            alt="Blue T-Shirt"
-            width={2560}
-            height={2560}
-            style={{ cursor: 'pointer' }}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          />
+          <Link href="/products/americano-cap">
+            <Image
+              src={
+                isHovered
+                  ? "/Americano-Royal-Foc1-scaled.jpg"
+                  : "/Americano-model-scaled.jpg"
+              }
+              alt="Blue T-Shirt"
+              width={2560}
+              height={2560}
+              style={{ cursor: "pointer" }}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            />
           </Link>
         </figure>
         <div className="card-body bg-gray-300 rounded-b  hover:shadow-lg">
-          <h2 className="card-title flex justify-center font-medium">Americano Cap</h2>
+          <h2 className="card-title flex justify-center font-medium">
+            Americano Cap
+          </h2>
           <div className="rating flex justify-center">
-          <RiStarSFill className="my-3 text-yellow-400 text-2xl"/>
-          <RiStarSFill className="my-3 text-yellow-400 text-2xl"/>
-          <RiStarSFill className="my-3 text-yellow-400 text-2xl"/>
-          <RiStarSFill className="my-3 text-yellow-400 text-2xl"/>
-          <RiStarSFill className="my-3 text-yellow-400 text-2xl"/>
+            <RiStarSFill className="my-3 text-yellow-400 text-2xl" />
+            <RiStarSFill className="my-3 text-yellow-400 text-2xl" />
+            <RiStarSFill className="my-3 text-yellow-400 text-2xl" />
+            <RiStarSFill className="my-3 text-yellow-400 text-2xl" />
+            <RiStarSFill className="my-3 text-yellow-400 text-2xl" />
           </div>
         </div>
       </div>
