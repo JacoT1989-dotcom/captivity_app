0bdd71ff272dfe8dcd7ffea8e4de3640e4f5e8f4
JacoT1989-dotcom <jacothiart52@gmail.com>
2025-01-16 22:25:45 +0200

    found the issue product grid wrapper needs to be rebuild

--- app/(user)/(DropDownCategories)/products/apparel/layout.tsx
+++ app/(user)/(DropDownCategories)/products/apparel/layout.tsx
@@ -1,7 +1,8 @@
 "use client";
+
 import React, { useEffect } from "react";
 import FilterSidebar from "./_sidebar/_components/FilterSidebar";
-import ProductGridWrapper from "./ProductGridWrapper";
+// import ProductGridWrapper from "./ProductGridWrapper";
 import { usePathname } from "next/navigation";
 import { FilterState } from "./_store/types";
 import { useCategoryStore } from "./_store/apparel-store";
@@ -33,29 +34,30 @@ export default function Layout({ children }: LayoutProps) {
   };
 
   return (
-    <div className="min-h-screen w-full relative">
-      <div className="relative z-30">{children}</div>
+    <div className="min-h-screen w-full">
+      <div className="w-full relative z-30">{children}</div>
 
-      <div className="flex w-full mt-6">
+      <div className="flex w-full px-2 sm:px-4 lg:px-8 mt-6">
         <aside className="hidden lg:block w-64 flex-shrink-0">
           <div className="sticky top-[76px]">
             <FilterSidebar
-              className="mr-6"
+              className="border-r border-gray-200"
               onFilterChange={handleFilterChange}
             />
           </div>
         </aside>
 
-        <main className="flex-1">
-          <div className="w-full max-w-[calc(100vw-32px)] lg:max-w-full mx-auto">
-            <ProductGridWrapper />
-          </div>
+        <main className="flex-1 lg:ml-6 w-full lg:max-w-[calc(100%-16rem)]">
+          {" "}
+          {/* Updated width calculation */}
+          <div className="w-full">{/* <ProductGridWrapper /> */}</div>
         </main>
       </div>
 
-      <div className="fixed bottom-0 right-0 p-6 z-50 lg:hidden">
-        <FilterSidebar onFilterChange={handleFilterChange} />
-      </div>
+      <FilterSidebar
+        className="lg:hidden"
+        onFilterChange={handleFilterChange}
+      />
     </div>
   );
 }
