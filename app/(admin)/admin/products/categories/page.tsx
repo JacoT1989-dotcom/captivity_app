723d2e122dc48461621bf416f3a75f58e8ccf0e7
Pencils0101001101001010 <155194934+Pencils0101001101001010@users.noreply.github.com>
2025-01-16 11:24:51 +0200

    starting_on_e-commerce_products_section

---
+++ app/(admin)/admin/products/categories/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      <h1>Create Categories.</h1>
+    </div>
+  );
+};
+
+export default page;
---
+++ app/(admin)/admin/products/inventory/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Inventory Page</div>;
+};
+
+export default page;
